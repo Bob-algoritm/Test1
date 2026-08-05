d576a3b20d93db8565df944fb6c0ce4486711f1b
Bob <joldosevbobur64@gmail.com>
2026-08-05 12:47:42 +0000

    File changes

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 import { Image } from "@/components/ui/image";
 
 const LOGO_URL =
-  "https://media.base44.com/images/public/6a6c41544c562d8bc4335ef5/52f4f72ec_pdf.png";
+  "https://media.base44.com/images/public/6a6c41544c562d8bc4335ef5/5d581112f_pdf.png";
 
 export default function Logo({ className = "w-9 h-9 rounded-xl" }) {
   return (
